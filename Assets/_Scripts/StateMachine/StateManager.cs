609a2b42404714ae45e511ffae2424ba659f3842
Aditi Verma <anvil557@gmail.com>
2026-01-06 22:27:44 +0530

    bug fix

--- Assets/_Scripts/StateMachine/StateManager.cs
+++ Assets/_Scripts/StateMachine/StateManager.cs
@@ -1,38 +1,37 @@
 using UnityEngine;
 
-public class StateManager : MonoBehaviour
-{
-  [Header("StateHandlers")]
-  [SerializeField] private Initialization initialization;
-  public static StateManager Instance;
-  public GameState currentState;
-  public GameState dealingState;
-  private void Awake()
-  {
-    Instance = this;
-  }
+public class StateManager : MonoBehaviour {
+    [Header("StateHandlers")]
+    [SerializeField] private Initialization initialization;
+    public static StateManager Instance;
+    private GameState currentState;
 
-  private void Start()
-  {
-    ChangeState(GameState.Initialization);
-  }
-  public void ChangeState(GameState newState)
-  {
-    Debug.Log($"Changing state to {newState}");
-    currentState = newState;
-    SetCurrentState();
-  }
-
-  public void SetCurrentState()
-  {
-    switch (currentState)
+    private void Awake() 
     {
-      case GameState.Initialization:
-        initialization.StartInitialization();
-        break;
-      case GameState.Dealing:
-        initialization.StartInitialization();
-        break;
+        Instance = this;
     }
-  }
+
+    private void Start()
+    {
+        ChangeState(GameState.Initialization);
+    }
+
+    public void ChangeState(GameState newState)
+    {
+        Debug.Log($"Changing state to {newState}");
+        currentState = newState;
+        SetCurrentState();
+    }
+
+    public void SetCurrentState()
+    {
+        switch (currentState)
+        {
+            case GameState.Initialization:
+                initialization.StartInitialization();
+                break;
+            case GameState.Dealing:
+                break;
+        }
+    }
 }
