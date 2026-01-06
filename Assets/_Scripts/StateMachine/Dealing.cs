b41690f1faad5b089dad6f25009ea30b6f768f93
Aditi Verma <anvil557@gmail.com>
2026-01-06 21:52:45 +0530

    dealing function nad left right calculation

--- Assets/_Scripts/StateMachine/Dealing.cs
+++ Assets/_Scripts/StateMachine/Dealing.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class Dealing : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private RectTransform left;
+    [SerializeField] private RectTransform right;
+    private void StartDealing()
     {
+        Vector2 leftPos = UniversalPositionsOfObjects.Instance.GetPositionFromCanvasCenter(left);
+        Vector2 rightPos = UniversalPositionsOfObjects.Instance.GetPositionFromCanvasCenter(right);
+        float distance = Vector2.Distance(leftPos, rightPos);
+        Debug.Log(distance);
+        float offset = distance / 12;
         
+    }
+    private void EndDealing()
+    {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/_Scripts/UniversalPositionsOfObjects.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniversalPositionsOfObjects : MonoBehaviour {
+    public static UniversalPositionsOfObjects Instance;
+
+    public RectTransform universalCenter;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    public Vector2 GetPositionFromCanvasCenter(RectTransform target) {
+        // Convert target's world position into local space of canvas
+        Vector3 localPos = universalCenter.InverseTransformPoint(target.position);
+
+        return new Vector2(localPos.x, localPos.y);
+    }
+}
